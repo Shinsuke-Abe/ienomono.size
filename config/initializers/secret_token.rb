5e3ca6248241aea42bc52b453cc2357027015ef3
Shinsuke Abe <mao.instantlife@gmail.com>
2013-02-14 22:15:16 +0900

    create rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IenomonoSize::Application.config.secret_token = 'af9805652fbb351437ae90897940037b23ac6ac9d85ecab1aaf7f7a09701a4b11483be116fe539812c937f52c7c27d88b5bc944ff5c14586926ed7bcdb2f7a2b'
