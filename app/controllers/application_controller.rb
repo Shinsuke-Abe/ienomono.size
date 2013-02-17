6763a6b6dbb4514fd459fb07bd879a6b9c44adac
Shinsuke Abe <mao.instantlife@gmail.com>
2013-02-17 17:41:07 +0900

    add layout for devise authenticate view.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  layout :layout
+
+  private
+  def layout
+    is_a?(Devise::SessionsController) ? "single" : "application"
+  end
 end
